0c3199edbe7ed77d25d642f08724c639426d3d57
alicimens <115820400+alicimens@users.noreply.github.com>
2025-04-09 22:08:52 +0300

    Delete Python çalışmaları/main.py

--- Python çalışmaları/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication  # Uygulama başlatmak için gereken sınıf
-from login import LoginPage  # Az önce oluşturduğumuz LoginPage sınıfını içe aktarıyoruz
-
-app = QApplication([])  # Uygulama nesnesi oluşturuyoruz (event loop başlatmak için şart)
-pencere = LoginPage()  # LoginPage sınıfından bir pencere adında nesne oluşturuyoruz
-
-pencere.show()  # Pencereyi ekranda gösteriyoruz
-
-app.exec_()  # PyQt5 uygulamasını çalıştırmak için event loop'u başlatıyoruz
